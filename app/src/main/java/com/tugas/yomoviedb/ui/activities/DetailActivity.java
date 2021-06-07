a23e0b3192ee54cf0b314829aba6747ea095e856
Richard Enrico <richardenrico02@gmail.com>
2021-06-08 00:10:14 +0800

    Add DetailActivity

---
+++ app/src/main/java/com/tugas/yomoviedb/ui/activities/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.tugas.yomoviedb.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tugas.yomoviedb.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
