fe285ba927e014c759bb57d38c67b1ab480a8df5
Richard Enrico <richardenrico02@gmail.com>
2021-06-10 08:34:15 +0800

    add new endpoint

--- app/src/main/java/com/tugas/yomoviedb/data/api/Service.java
+++ app/src/main/java/com/tugas/yomoviedb/data/api/Service.java
@@ -1,7 +1,9 @@
 package com.tugas.yomoviedb.data.api;
 
-import com.tugas.yomoviedb.data.models.TvShow;
-import com.tugas.yomoviedb.data.models.TvShowResponse;
+import com.tugas.yomoviedb.data.models.movie.Movie;
+import com.tugas.yomoviedb.data.models.movie.MovieResponse;
+import com.tugas.yomoviedb.data.models.tvshow.TvShow;
+import com.tugas.yomoviedb.data.models.tvshow.TvShowResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -10,20 +12,40 @@ import retrofit2.http.Query;
 
 public interface Service {
     @GET("tv/{sort_by}")
-    Call<TvShowResponse> getResults(
+    Call<TvShowResponse> getTvResults(
             @Path("sort_by") String sortBy,
             @Query("api_key") String apiKey,
             @Query("page") int page
     );
 
     @GET("tv/{id}")
-    Call<TvShow> getDetail(
+    Call<TvShow> getTvDetail(
             @Path("id") int id,
             @Query("api_key") String apiKey
     );
 
     @GET("search/tv")
-    Call<TvShowResponse> search(
+    Call<TvShowResponse> searchTv(
+            @Query("api_key") String apiKey,
+            @Query("query") String query,
+            @Query("page") int page
+    );
+
+    @GET("movie/{sort_by}")
+    Call<MovieResponse> getMovieResults(
+            @Path("sort_by") String sortBy,
+            @Query("api_key") String apiKey,
+            @Query("page") int page
+    );
+
+    @GET("movie/{id}")
+    Call<Movie> getMovieDetail(
+            @Path("id") int id,
+            @Query("api_key") String apiKey
+    );
+
+    @GET("search/movie")
+    Call<MovieResponse> searchMovie(
             @Query("api_key") String apiKey,
             @Query("query") String query,
             @Query("page") int page
