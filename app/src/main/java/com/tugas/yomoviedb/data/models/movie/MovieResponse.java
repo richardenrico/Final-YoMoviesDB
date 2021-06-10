099f617adf2a15670c62707ccd8db9e28bb198a0
Richard Enrico <richardenrico02@gmail.com>
2021-06-10 08:33:47 +0800

    add "magic" to MovieFragment.java, MovieResponse.java

--- app/src/main/java/com/tugas/yomoviedb/data/models/movie/MovieResponse.java
+++ app/src/main/java/com/tugas/yomoviedb/data/models/movie/MovieResponse.java
@@ -1,4 +1,34 @@
 package com.tugas.yomoviedb.data.models.movie;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class MovieResponse {
+    @SerializedName("page")
+    private int page;
+    @SerializedName("total_results")
+    private int totalResluts;
+    @SerializedName("total_pages")
+    private int totalPages;
+    @SerializedName("results")
+    @Expose
+    private List<Movie> movieResult;
+
+    public List<Movie> getMovieResult() {
+        return movieResult;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getTotalResluts() {
+        return totalResluts;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
 }
--- app/src/main/java/com/tugas/yomoviedb/ui/fragments/MovieFragment.java
+++ app/src/main/java/com/tugas/yomoviedb/ui/fragments/MovieFragment.java
@@ -1,27 +1,196 @@
 package com.tugas.yomoviedb.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.appcompat.widget.SearchView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 import com.tugas.yomoviedb.R;
+import com.tugas.yomoviedb.data.api.repository.MovieRepository;
+import com.tugas.yomoviedb.data.api.repository.TvShowRepository;
+import com.tugas.yomoviedb.data.api.repository.callback.OnMovieCallback;
+import com.tugas.yomoviedb.data.api.repository.callback.OnMovieSearchCallback;
+import com.tugas.yomoviedb.data.api.repository.callback.OnTvSearchCallback;
+import com.tugas.yomoviedb.data.api.repository.callback.OnTvShowCallback;
+import com.tugas.yomoviedb.data.models.movie.Movie;
+import com.tugas.yomoviedb.data.models.tvshow.TvShow;
+import com.tugas.yomoviedb.ui.activities.DetailActivity;
+import com.tugas.yomoviedb.ui.adapters.MovieAdapter;
+import com.tugas.yomoviedb.ui.adapters.TvShowAdapter;
+import com.tugas.yomoviedb.ui.adapters.clicklistener.OnMovieClickListener;
+import com.tugas.yomoviedb.ui.adapters.clicklistener.OnTvShowClickListener;
 
-public class MovieFragment extends Fragment {
+import java.util.List;
+
+public class MovieFragment extends Fragment
+        implements OnMovieClickListener,
+        SwipeRefreshLayout.OnRefreshListener,
+        SearchView.OnQueryTextListener{
+
+    private SwipeRefreshLayout refreshLayout;
+    private RecyclerView recyclerView;
+    private MovieAdapter adapter;
+    private MovieRepository repository;
+    private boolean isFetching;
+    private int currentPage = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        MenuItem item = menu.findItem(R.id.item_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        setSearchView(searchView);
+
+    }
+
+    private void setSearchView(SearchView searchView) {
+        searchView.setQueryHint("Type here...");
+        searchView.setOnQueryTextListener(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false);
+        // Inflate the layout for this
+        View view = inflater.inflate(R.layout.fragment_movie, container, false);
+
+        refreshLayout = view.findViewById(R.id.swl_movie);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        repository = MovieRepository.getInstance();
+        getRepositoryData("", currentPage);
+        onScrollListener();
+        refreshLayout.setOnRefreshListener(this);
+
+        return view;
+    }
+
+    private void onScrollListener() {
+        final GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 3);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                int totalItem = layoutManager.getItemCount();
+                int visibleItem = layoutManager.getChildCount();
+                int firstVisibleItem = layoutManager.findFirstVisibleItemPosition();
+                if (firstVisibleItem + visibleItem >= totalItem / 2) {
+                    if (!isFetching) {
+                        isFetching = true;
+                        currentPage++;
+                        getRepositoryData("", currentPage);
+                        isFetching = false;
+                    }
+                }
+            }
+        });
+    }
+
+    private void getRepositoryData(String query, int page) {
+        isFetching = true;
+        if (query.equals("")) {
+            repository.getMovie(getBundle(), page, new OnMovieCallback() {
+                @Override
+                public void onSuccess(int page, List<Movie> movieList) {
+                    if (adapter == null) {
+                        adapter = new MovieAdapter(movieList);
+                        adapter.setClickListener(MovieFragment.this);
+                        adapter.notifyDataSetChanged();
+                        recyclerView.setAdapter(adapter);
+                    } else {
+                        adapter.appendList(movieList);
+                    }
+                    currentPage = page;
+                    isFetching = false;
+                    refreshLayout.setRefreshing(false);
+                }
+
+                @Override
+                public void onFailure(String message) {
+                    Log.d("ERROR", "onFailure:" + message);
+                }
+            });
+        } else {
+            repository.searchMovie(query, page, new OnMovieSearchCallback() {
+                @Override
+                public void onSuccess(List<Movie> movieList, String msg, int page) {
+                    if (adapter == null) {
+                        adapter = new MovieAdapter(movieList);
+                        adapter.setClickListener(MovieFragment.this);
+                        adapter.notifyDataSetChanged();
+                        recyclerView.setAdapter(adapter);
+                    } else {
+                        adapter.appendList(movieList);
+                    }
+                    currentPage = page;
+                    isFetching = false;
+                    refreshLayout.setRefreshing(false);
+                }
+
+                @Override
+                public void onFailure(String msg) {
+
+                }
+            });
+        }
+    }
+
+    private String getBundle() {
+        if (getArguments() != null) {
+            return getArguments().getString("SORT_BY");
+        }
+        return "now_playing";
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (newText.length() > 0) {
+            adapter = null;
+            getRepositoryData(newText, currentPage);
+        } else {
+            adapter = null;
+            getRepositoryData("", currentPage);
+        }
+        return true;
+    }
+
+    @Override
+    public void onRefresh() {
+        adapter = null;
+        getRepositoryData("", currentPage);
+    }
+
+    @Override
+    public void onClick(Movie movie) {
+        Intent intent = new Intent(getContext(), DetailActivity.class);
+        intent.putExtra("ID", movie.getId());
+        intent.putExtra("SELECTED_FRAGMENT", getBundle());
+        intent.putExtra("TYPE", "MOVIE");
+        startActivity(intent);
     }
 }
